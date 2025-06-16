2bbb1cb385868335018b1cf0bdb2e5db9e671e51
wasim822 <wasimborhot@gmail.com>
2025-06-16 17:06:45 -0600

    first commit

---
+++ groupProject1/Program.cs
@@ -0,0 +1,107 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+
+    public abstract class Appliance
+    {
+        public string ItemNumber { get; set; }
+        public string Brand { get; set; }
+        public int Quantity { get; set; }
+        public int Wattage { get; set; }
+        public string Color { get; set; }
+        public double Price { get; set; }
+
+        public Appliance(string itemNumber, string brand, int quantity, int wattage, string color, double price)
+        {
+            ItemNumber = itemNumber;
+            Brand = brand;
+            Quantity = quantity;
+            Wattage = wattage;
+            Color = color;
+            Price = price;
+        }
+
+        public abstract override string ToString();
+    }
+
+
+    public class Refrigerator : Appliance
+    {
+        public int NumberOfDoors { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public Refrigerator(string itemNumber, string brand, int quantity, int wattage, string color, double price,
+            int numberOfDoors, int height, int width)
+            : base(itemNumber, brand, quantity, wattage, color, price)
+        {
+            NumberOfDoors = numberOfDoors;
+            Height = height;
+            Width = width;
+        }
+
+        public override string ToString()
+        {
+            return $"Item Number: {ItemNumber}\nBrand: {Brand}\nQuantity: {Quantity}\nWattage: {Wattage}\nColor: {Color}\nPrice: {Price}\nNumber of Doors: {NumberOfDoors}\nHeight: {Height}\nWidth: {Width}";
+        }
+    }
+
+
+    public class Vacuum : Appliance
+    {
+        public string Grade { get; set; }
+        public int BatteryVoltage { get; set; }
+
+        public Vacuum(string itemNumber, string brand, int quantity, int wattage, string color, double price,
+            string grade, int batteryVoltage)
+            : base(itemNumber, brand, quantity, wattage, color, price)
+        {
+            Grade = grade;
+            BatteryVoltage = batteryVoltage;
+        }
+
+        public override string ToString()
+        {
+            return $"Item Number: {ItemNumber}\nBrand: {Brand}\nQuantity: {Quantity}\nWattage: {Wattage}\nColor: {Color}\nPrice: {Price}\nGrade: {Grade}\nBattery Voltage: {BatteryVoltage}";
+        }
+    }
+
+    public class Microwave : Appliance
+    {
+        public double Capacity { get; set; }
+        public string RoomType { get; set; }
+
+        public Microwave(string itemNumber, string brand, int quantity, int wattage, string color, double price,
+            double capacity, string roomType)
+            : base(itemNumber, brand, quantity, wattage, color, price)
+        {
+            Capacity = capacity;
+            RoomType = roomType;
+        }
+
+        public override string ToString()
+        {
+            return $"Item Number: {ItemNumber}\nBrand: {Brand}\nQuantity: {Quantity}\nWattage: {Wattage}\nColor: {Color}\nPrice: {Price}\nCapacity: {Capacity}\nRoom Type: {RoomType}";
+        }
+    }
+
+
+    public class Dishwasher : Appliance
+    {
+        public string Feature { get; set; }
+        public string SoundRating { get; set; }
+
+        public Dishwasher(string itemNumber, string brand, int quantity, int wattage, string color, double price,
+            string feature, string soundRating)
+            : base(itemNumber, brand, quantity, wattage, color, price)
+        {
+            Feature = feature;
+            SoundRating = soundRating;
+        }
+
+        public override string ToString()
+        {
+            return $"Item Number: {ItemNumber}\nBrand: {Brand}\nQuantity: {Quantity}\nWattage: {Wattage}\nColor: {Color}\nPrice: {Price}\nFeature: {Feature}\nSoundRating: {SoundRating}";
+        }
+    }
